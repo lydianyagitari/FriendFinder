1caa3eb6f97c62d57e86f2dc82b6953bf4a70521
Lydia Innocents <mzinnocents96@gmail.com>
2018-12-22 22:17:12 -0600

    links

---
+++ app/Routing/htmlRoutes.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "FriendFinder\app\Public\survey.html"));
+    });
+
+    /*app.get("/reserve", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    });*/
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "FriendFinder\app\Public\home.html"));
+    });
+};
---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+const friends = [
+    {
+      "name":"Eva",
+      "photo":"https://scontent-msp1-1.xx.fbcdn.net/v/t1.0-9/13133331_274037506272691_3760997143431941707_n.jpg?_nc_cat=101&_nc_ht=scontent-msp1-1.xx&oh=659461440afcda9ded0f5cd49c8905ec&oe=5C9C54A5",
+      "scores":[ 3,4,5,4, 3 ] 
+    },
+    {
+      "name":"Hellen",
+      "photo":"https://scontent-msp1-1.xx.fbcdn.net/v/t1.0-9/14322778_342213009455140_7390660637225266922_n.jpg?_nc_cat=110&_nc_ht=scontent-msp1-1.xx&oh=9e574fbc3009ebedc381e172f127d0d4&oe=5CD3B3E6",
+      "scores":[ 4, 1, 5, 3, 2  ]
+    },
+    {
+      "name":"Emily",
+      "photo":"https://scontent-msp1-1.xx.fbcdn.net/v/t1.0-9/15726443_400199940323113_8355702987337124848_n.jpg?_nc_cat=108&_nc_ht=scontent-msp1-1.xx&oh=87f34f29a67f6323499b818c50fd3ddd&oe=5CD1496D",
+      "scores":[ 2, 3, 2, 2, 3]
+    },
+    {
+      "name":"Edina",
+      "photo":"https://scontent-msp1-1.xx.fbcdn.net/v/t1.0-9/16807103_426620211014419_3254716531100445519_n.jpg?_nc_cat=103&_nc_ht=scontent-msp1-1.xx&oh=a1a16c7ec25863adb5ad9131704cb5c7&oe=5CA23E90",
+      "scores":[ 5, 4, 5, 2, 4 ]
+    },
+    {
+      "name":"Sheryln",
+      "photo":"https://scontent-msp1-1.xx.fbcdn.net/v/t1.0-9/14705617_355628968113544_217832948141121757_n.jpg?_nc_cat=106&_nc_ht=scontent-msp1-1.xx&oh=76221bac4f0cba12690dd0c69afbeebf&oe=5CD5F271",
+      "scores":[ 2, 3, 4, 5, 1 ]
+    },
+   
+    ];
+    
+    module.exports = friends;
