8dac2db5620c69826e139d5b6b81a5683cfe0a43
Lydia Innocents <mzinnocents96@gmail.com>
2019-01-02 21:25:41 -0600

    edit

--- app/Routing/htmlRoutes.js
+++ app/Routing/htmlRoutes.js
@@ -1,25 +1,16 @@
 var path = require("path");
 
 
-// ===============================================================================
 // ROUTING
-// ===============================================================================
+
 
 module.exports = function (app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-    app.get("/survey", function (req, res) {
+    app.get("FriendFinder/app/Public/survey.html", function (req, res) {
         res.sendFile(path.join(__dirname, "FriendFinder\app\Public\survey.html"));
     });
 
-    /*app.get("/reserve", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/reserve.html"));
-    });*/
-
-    // If no matching route is found default to home
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "FriendFinder\app\Public\home.html"));
     });
